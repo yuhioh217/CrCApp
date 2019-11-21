920e3fedcb8a3b05eb9d2eb29e1ac438affb43e2
KE Jiang <yuhioh217@gmail.com>
2019-11-21 16:42:54 +0800

    first commit

---
+++ WebViewExample/ViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController,WKNavigationDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //load remote url
+        let url = URL(string: "https://d1xvbqs3gqmypi.cloudfront.net/")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
