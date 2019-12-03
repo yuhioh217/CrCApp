17b41a5d1baeaf313deabaabe111cad68a7c9680
KE Jiang <yuhioh217@gmail.com>
2019-12-03 10:11:17 +0800

    Disable the idle timer and set the brightness always 1.0

--- WebViewExample/AppDelegate.swift
+++ WebViewExample/AppDelegate.swift
@@ -9,6 +9,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UIApplication.shared.isStatusBarHidden = true
+        UIApplication.shared.isIdleTimerDisabled = true
+        UIScreen.main.brightness = CGFloat(1.0)
         return true
     }
 
